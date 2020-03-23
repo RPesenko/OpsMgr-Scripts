6ba8caf016df81910134cad77d5c872f4821c83c
RPesenko <56746582+RPesenko@users.noreply.github.com>
2020-03-23 15:43:31 -0500

    Posted ExportMPs

---
+++ ExportMP/ExportMPs.ps1
@@ -0,0 +1,58 @@
+﻿<# script to export all MPs
+ -Servername = MS to use (default to localhost)
+ -Sealed = Export sealed and unsealed MP (default to unsealed only)
+ -Folder = Folder to export MP to (default to C:\ManagementPacks)
+
+ Version: 2020/03/23
+#>
+param 
+(
+    $ServerName,
+    [String]$Sealed = $false,
+    [String]$Folder = "C:\ManagementPacks"
+)
+
+# Default to Localhost if Management Server is not specified
+if(!$ServerName) {
+    $localserver = Get-Childitem env:computername |Select-Object value
+    $ServerName = $localserver.value
+}
+
+# Default to only unsealed MP unless option to export Sealed MP is specified
+if($Sealed.ToLower() -eq "true") {
+    $GetAll = $True
+}
+
+# Specify location to store the exported MP in
+$Date = Get-Date -Format “yyyy-MM-dd”
+$TodaysFolder = $Folder + "\" + $Date
+
+# Connect to Data Access Service to export MP
+TRY {
+    add-pssnapin “Microsoft.EnterpriseManagement.OperationsManager.Client”
+    set-location “OperationsManagerMonitoring::” 
+    new-managementGroupConnection -ConnectionString:$ServerName |out-null
+    set-location $ServerName
+
+    if($GetAll){
+        $mps = get-SCOMmanagementpack 
+    }
+    Else {
+        $mps = get-SCOMmanagementpack | where-object {$_.Sealed -eq $false}
+    }
+
+    # Clean up folder if it already exists
+    If(Test-Path $TodaysFolder) {
+        Remove-item $TodaysFolder -Recurse -Force
+    }
+    New-Item $TodaysFolder -type directory -force |out-null
+    
+    foreach ($mp in $mps) {
+        export-SCOMManagementpack -managementpack $mp -path $TodaysFolder
+    }
+    Write-Host "Export complete." -ForegroundColor Green
+}
+Catch {
+    Write-Host "Cannot connect to Data Access Service on $ServerName." -ForegroundColor Red
+}
+Set-location C:\
---
+++ ExportMP/Readme.md
@@ -0,0 +1,12 @@
+# ExportMPs.ps1
+
+This is a script that will export all the Management Packs in a SCOM Managment Group.
+The script uses the folowing parameters:
+
+ - _Servername_ = Name of the Management Server to connect to.  If not is specified, the script will connect to the local machine and attempt to load the OpsMgr Command Shell.  
+ - _Sealed_ = Set value to "True" if you want to export both sealed and unsealed Management Packs.  The script will default to only export unsealed MP.
+ - _Folder_ = Specify the folder you want to export Management Packs to.  If no folder is specified, the script will create a new export root folder at "C:\ManagementPacks".  
+
+ A child folder with the day's date (in YYYY-MM-DD format) will be created at the parent folder specfied above.  If a child folder for today's date already exists, it will be overwritten.
+
+ Version: 2020/03/23
