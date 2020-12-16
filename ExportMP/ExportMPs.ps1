6e5ac6c45b3d981cef4504e7a3bf1856f5fc0fca
RPesenko <RPesenko@live.com>
2020-12-16 16:27:01 -0600

    Improved connection logic

--- ExportMP/ExportMPs.ps1
+++ ExportMP/ExportMPs.ps1
@@ -1,22 +1,87 @@
-﻿<# script to export all MPs
+﻿<#
+ExportMPs.ps1
+    Version: 1.1    
+    Release 2020/12/16
+
+Script to export all Management Packs
  -Servername = MS to use (default to localhost)
  -Sealed = Export sealed and unsealed MP (default to unsealed only)
  -Folder = Folder to export MP to (default to C:\ManagementPacks)
-
- Version: 2020/03/23
 #>
 param 
 (
-    $ServerName,
     [String]$Sealed = $false,
-    [String]$Folder = "C:\ManagementPacks"
+    [String]$MSConnection,
+    [String]$Folder = "C:\SCOMFiles\ManagementPacks"
 )
 
-# Default to Localhost if Management Server is not specified
-if(!$ServerName) {
-    $localserver = Get-Childitem env:computername |Select-Object value
-    $ServerName = $localserver.value
+# ===============================================================================================
+# functions
+function GetSCOMModule ($MgmtServer){
+
+    $Server = $MgmtServer
+
+    Try {
+        $SCOMPowerShellKey = "HKLM:\SOFTWARE\Microsoft\System Center Operations Manager\12\Setup\Powershell\V2"
+        $SCOMModulePath = Join-Path (Get-ItemProperty $SCOMPowerShellKey).InstallDirectory "OperationsManager"
+        Import-module $SCOMModulePath -ErrorAction Stop
+        New-DefaultManagementGroupConnection $Server -ErrorAction Stop
+    }
+    Catch {
+        Write-Host "SCOM Console not installed or OperationsManager module not found" -ForegroundColor Red
+        Write-Host "Try running script as local administrator or from OperationsManager shell session." -ForegroundColor Red
+        Exit
+    }
 }
+
+function ConnectToSDK ($MS) {
+
+    $MgmtServer = $MS
+
+    Try {
+        New-DefaultManagementGroupConnection $MgmtServer -EA stop
+    }    
+    Catch {
+        GetSCOMModule $MgmtServer
+    }
+}
+
+function CheckMSConnect {
+    # Check connection to Management Group
+    $isConnected = Get-SCManagementGroupConnection
+    If (!$isConnected){
+        #  If not connected, connect to the required MS, or default to localhost
+        If (!$MSConnection){
+            ConnectToSDK "Localhost"
+        }
+        Else {
+            ConnectToSDK $MSConnection
+        }
+    }
+    Else {
+        #  If already connected, use existing connection or change connection to the required MS
+        If ((!$MSConnection) -or ($isConnected.ManagementServerName -eq $MSConnection)){
+            $connected = $isConnected |? {$_.IsActive}
+            # Check if the existing connection is active, if not, set it active.
+            If ($connected.ManagementServerName -eq $MSConnection){
+                Write-host "Connected to" $Connected.ManagementGroupName "on" $Connected.ManagementServerName -ForegroundColor Magenta
+            }
+            Else {
+                $isConnected |? {$_.ManagementServerName -eq $MSConnection} |Set-SCManagementGroupConnection
+                $connected = $isConnected |? {$_.IsActive}
+                Write-host "Connected to" $Connected.ManagementGroupName "on" $Connected.ManagementServerName -ForegroundColor Magenta
+            }
+        }
+        Else {
+            ConnectToSDK $MSConnection
+        }
+    }
+}
+#---------------------------------------------------------------------
+# MAIN
+
+# CHeck for established connection to Management Server  
+CheckMSConnect
 
 # Default to only unsealed MP unless option to export Sealed MP is specified
 if($Sealed.ToLower() -eq "true") {
@@ -29,30 +94,28 @@ $TodaysFolder = $Folder + "\" + $Date
 
 # Connect to Data Access Service to export MP
 TRY {
-    add-pssnapin “Microsoft.EnterpriseManagement.OperationsManager.Client”
-    set-location “OperationsManagerMonitoring::” 
-    new-managementGroupConnection -ConnectionString:$ServerName |out-null
-    set-location $ServerName
-
     if($GetAll){
         $mps = get-SCOMmanagementpack 
+        Write-Host "Exporting all management packs." -ForegroundColor Magenta
     }
     Else {
         $mps = get-SCOMmanagementpack | where-object {$_.Sealed -eq $false}
+        Write-Host "Exporting only unsealed management packs." -ForegroundColor Magenta
     }
-
-    # Clean up folder if it already exists
-    If(Test-Path $TodaysFolder) {
-        Remove-item $TodaysFolder -Recurse -Force
-    }
-    New-Item $TodaysFolder -type directory -force |out-null
-    
-    foreach ($mp in $mps) {
-        export-SCOMManagementpack -managementpack $mp -path $TodaysFolder
-    }
-    Write-Host "Export complete." -ForegroundColor Green
 }
 Catch {
     Write-Host "Cannot connect to Data Access Service on $ServerName." -ForegroundColor Red
 }
-Set-location C:\
+
+# Clean up folder if it already exists
+If(Test-Path $TodaysFolder) {
+    Remove-item $TodaysFolder -Recurse -Force
+}
+New-Item $TodaysFolder -type directory -force |out-null
+
+# Save each management pack to target folder
+foreach ($mp in $mps) {
+    export-SCOMManagementpack -managementpack $mp -path $TodaysFolder
+}
+
+Write-Host "Export complete." -ForegroundColor Green
--- ExportMP/Readme.md
+++ ExportMP/Readme.md
@@ -1,16 +1,20 @@
 # ExportMPs.ps1
-
+    Version: 2.0
+    Release 2020/12/16
+  
 This is a script that will export all the Management Packs in a SCOM Managment Group.
 The script uses the folowing parameters:
 
- - _Servername_ = Name of the Management Server to connect to.  If not is specified, the script will connect to the local machine and attempt to load the OpsMgr Command Shell.  
+ - _MSConnection_ = Name of the Management Server to connect to.  If not is specified, the script will connect to the local machine and attempt to load the OpsMgr Command Shell.  
  - _Sealed_ = Set value to "True" if you want to export both sealed and unsealed Management Packs.  The script will default to only export unsealed MP.
- - _Folder_ = Specify the folder you want to export Management Packs to.  If no folder is specified, the script will create a new export root folder at "C:\ManagementPacks".  
+ - _Folder_ = Specify the folder you want to export Management Packs to.  If no folder is specified, the script will create a new export root folder at "C:\SCOMFIles\ManagementPacks".  
 
  A child folder with the day's date (in YYYY-MM-DD format) will be created at the parent folder specfied above.  If a child folder for today's date already exists, it will be overwritten.
-
- Version: 2020/03/23
 
  ### Download Script Here 
  https://github.com/RPesenko/OpsMgr-Scripts/blob/master/ExportMP/ExportMPs.ps1
  
+Change Log  
+2.0: Improvements to MG connection  
+1.1: Updates to documentation   
+1.0: Initial release  
