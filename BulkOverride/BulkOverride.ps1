b12423ed10589ed9b2c36d1d9729676a43bea717
RPesenko <RPesenko@live.com>
2020-12-23 20:40:05 -0600

    In Progress

--- BulkOverride/BulkOverride.ps1
+++ BulkOverride/BulkOverride.ps1
@@ -5,21 +5,56 @@ BulkOveride.ps1
 Runs Get-SCOMEffectiveMonitoringConfiguration against an agent, or takes a previously generated CSV file, and presents all the currently enabled rules and monitors
  in a PowerShell Gridview object.  Enabled rules and monitors can be selected from the gridview and a single override MP can be generated to disable the selected workflows. 
 #>
-
+[CmdletBinding(DefaultParameterSetName = 'GetConfig')]
 Param (
-    [Parameter(ParameterSetName='UseExisting')][string] $ConfigFile,
-    [Parameter(Mandatory=$true,ParameterSetName='GetConfig')][string] $AgentFQDN,
+    [Parameter(Mandatory=$true,ParameterSetName='UseExisting',HelpMessage='Enter a configuration file to use.')][string] $ConfigFile,
+    [Parameter(Mandatory=$true,ParameterSetName='GetConfig',HelpMessage='Enter an agent FQDN to use.')][string] $AgentFQDN,
     [Parameter(ParameterSetName='GetConfig')][string] $MS,
     [Parameter(ParameterSetName='GetConfig')][string] $FolderPath = "C:\SCOMFiles\BulkOverride\"
 )
 
 # ===============================================================================================
 # functions
+function GetWorkflowType {
+    Param (
+        [string] $type,
+        [string] $alerts,
+        [string] $ID,
+        [string] $description
+    )
+    If ($type -eq 'rule'){
+        If ($alerts -eq "true") {
+            Return "Alert Rule"
+        }
+        Else {
+            If (($description -like "*collect*")-or ($ID -like "*collect*")) {
+                Return "Collection Rule"
+            }
+            Else {
+                Return "Rule"
+            }
+        }
+    } 
+    # Must be monitor, specify type
+    Else {
+        Switch -Regex ($ID) {
+            "AvailabilityState$" {Return "Rollup Monitor"}
+            "ConfigurationState$" {Return "Rollup Monitor"}
+            "PerformanceState$" {Return "Rollup Monitor"}
+            "SecurityState$" {Return "Rollup Monitor"}
+            "EntityState$" {Return "Rollup Monitor"}
+            "Rollup$" {Return "Rollup Monitor"}
+            "Aggregate" {Return "Aggregate Monitor"}
+            "DependencyMonitor$" {Return "Dependency Monitor"}
+            default {Return "Unit Monitor"}
+        } 
+    }
+}
+
 function GetRawCSV {
     param (
         $FileName 
     )
-    
     Try {
         $RawCSV = Get-Content $FileName -ErrorAction stop|sort |select -Skip 1 
     }
@@ -28,7 +63,6 @@ function GetRawCSV {
         Write-host $_.Exception.Message -ForegroundColor Red
         Exit
     }
-
     return $RawCSV
 }
 
@@ -99,12 +133,12 @@ function CheckMSConnect {
             $connected = $isConnected |? {$_.IsActive}
             # Check if the existing connection is active, if not, set it active.
             If ($connected.ManagementServerName -eq $MS){
-                Write-host "Connected to" $Connected.ManagementGroupName "on" $Connected.ManagementServerName -ForegroundColor Magenta
+                Write-host "Connected to" $Connected.ManagementGroupName "on" $Connected.ManagementServerName -ForegroundColor Cyan
             }
             Else {
                 $isConnected |? {$_.ManagementServerName -eq $MS} |Set-SCManagementGroupConnection
                 $connected = $isConnected |? {$_.IsActive}
-                Write-host "Connected to" $Connected.ManagementGroupName "on" $Connected.ManagementServerName -ForegroundColor Magenta
+                Write-host "Connected to" $Connected.ManagementGroupName "on" $Connected.ManagementServerName -ForegroundColor Cyan
             }
         }
         Else {
@@ -127,9 +161,45 @@ Else {
     #Generate Effective Configuration CSV
     $Hostname = ($AgentFQDN.Split('.'))[0]
     $output = GetConfig $FolderPath $AgentFQDN
+    Start-Sleep 4
 }
 
 # Get output of configuration CSV
-Write-Host "Formatting Configuration Information for $Hostname " -ForegroundColor Cyan
+Write-Host "Reading Configuration Information for $Hostname " -ForegroundColor Cyan
 $lines = GetRawCSV $output
-$total = $lines.Count
+
+# Needed for Progress bar
+$total = $lines.Count 
+$i = 0
+
+$Workflows = @()
+$lines | foreach {
+    # Progress bar
+    $i += 1
+    $progress = ($i/$total)*100
+    Write-Progress -Activity "Loading Workflows" -PercentComplete $progress
+
+    $values = $null
+    $values = ($_).Split('|')  
+
+    # Skip lines that wrapped from previous monitor description and workflows that are disabled
+    # Otherwise, get workflow configuration
+    If (($values[0] -match '\w\.\w') -and ($values[3] -ne $false)) {
+        $workflow = [PSCustomObject]@{
+            Class = $values[0]
+            Instance = $values[1]
+            WorkflowID = $values[2]
+            Enabled = $values[3]
+            MakesAlert =  $values[4]
+            AlertSev = $values[5]
+            AlertPri = $values[6]
+            WorkflowType = (GetWorkflowType $values[7] $values[4] $values[2] $values[8] )
+            Description = $values[8]
+            Overridden = $values[9]
+        }
+        $Workflows += $workflow
+    }
+}
+# Display Enabled workflow information
+$selected = $Workflows | Select Class, Instance, WorkflowID, WorkflowType, MakesAlert |Out-GridView -OutputMode Multiple
+$selected |Out-GridView -Title "These were selected"
